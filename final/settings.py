0087a093623d73235caf7643ee7eb8f1c87c32b0
User <user@localhost.localdomain>
2017-05-05 04:38:00 -0400

    initial Resource and Tag model classes

--- final/settings.py
+++ final/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.ReservationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Resource(models.Model):
+  # name of the resource
+  name = models.CharField(max_length=200)
+
+  # availability hours (will be any two times within the same day, e.g. 9am - 5pm)
+  start_time = models.DateTimeField('available start time')
+  end_time = models.DateTimeField('availabile end time')
+
+  def __str__(self):
+    return self.name
+
+# set of tags that describe the type of resource
+class Tag(models.Model):
+  name = models.CharField(max_length=200)
+  resources = models.ManyToManyField(Resource)
+
+  def __str__(self):
+    return self.name
